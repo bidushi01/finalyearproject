868e43c73746e17ebcc863ca1409bd91da29790b
bidushi gautam <gautambidushi4@gmail.com>
2026-07-04 14:07:42 +0545

    Update IEmailService.cs
    change needed

--- finalyearproject.Data/Helper/IEmailService.cs
+++ finalyearproject.Data/Helper/IEmailService.cs
@@ -1,16 +1,4 @@
-﻿//namespace finalyearproject.Data.Helper
-//{
-//    public interface IEmailService
-//    {
-//        Task<bool> SendOTPEmailAsync(string toEmail, string otpCode);
-//        Task<bool> SendApprovalEmailAsync(string toEmail, string username, bool isApproved);
-//        Task<bool> SendPasswordResetEmailAsync(string toEmail, string resetLink);
-
-
-//    }
-//}
-
-namespace finalyearproject.Data.Helper
+﻿namespace finalyearproject.Data.Helper
 {
     public interface IEmailService
     {
